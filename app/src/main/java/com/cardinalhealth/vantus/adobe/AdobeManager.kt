965112a113afcfa059e91efa536bcdceb8d093e1
cah-shivaprasad-bhat <shivaprasad.bhat@cardinalhealth.com>
2025-07-08 12:48:29 +0530

    Explore 2

--- app/src/main/java/com/cardinalhealth/vantus/adobe/AdobeManager.kt
+++ app/src/main/java/com/cardinalhealth/vantus/adobe/AdobeManager.kt
@@ -8,6 +8,7 @@ import com.adobe.marketing.mobile.Identity
 import com.adobe.marketing.mobile.LoggingMode
 import com.adobe.marketing.mobile.MobileCore
 import com.adobe.marketing.mobile.Target
+import com.cardinalhealth.vantus.App
 import javax.inject.Inject
 
 class AdobeManager @Inject constructor(
@@ -20,19 +21,8 @@ class AdobeManager @Inject constructor(
     fun init() {
         MobileCore.setApplication(context.applicationContext as Application)
         MobileCore.setLogLevel(LoggingMode.VERBOSE)
-        MobileCore.configureWithAppID("42925fc841db/9da9ec012a62/launch-0f1f5a8ce051-development")
         try {
-            val extensions = listOf(
-                Analytics.EXTENSION,
-                Identity.EXTENSION,
-                Target.EXTENSION
-            )
-            MobileCore.registerExtensions(
-                extensions
-            ) {
-                System.out.println("Successfully registered Adobe extensions")
-            }
-
+            MobileCore.initialize(context.applicationContext as Application, "42925fc841db/9da9ec012a62/launch-0f1f5a8ce051-development")
             Identity.getExperienceCloudId {
                 mid = it
             }
